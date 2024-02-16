28dd1c92a74de3d6f7e9759364bf33c108df54a8
Luiz Federico <luizrafa.federico@gmail.com>
2024-02-16 17:47:37 -0500

    parsing: payload.json file

--- src/app/check-list-page/check-list-page.component.ts
+++ src/app/check-list-page/check-list-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import * as data from '../shared/services/payload.json';
 
 @Component({
   selector: 'app-check-list-page',
   templateUrl: './check-list-page.component.html',
-  styleUrls: ['./check-list-page.component.scss']
+  styleUrls: ['./check-list-page.component.scss'],
 })
 export class CheckListPageComponent implements OnInit {
+  constructor() {
+    const payload = JSON.parse(JSON.stringify(data));
 
-  constructor() { }
-
-  ngOnInit(): void {
+    console.log('payload =>', payload);
   }
 
+  ngOnInit(): void {}
 }
